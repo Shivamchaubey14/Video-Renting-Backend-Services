8c2274b627e2b3178a0f9741ca778d13618e7de1
Shivam Kumar Chaubey <shivamc36@gmail.com>
2026-05-07 10:44:02 +0530

    Implemented an async validation

--- models/User.js
+++ models/User.js
@@ -1,7 +1,29 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    age: Number,
-    email: {type: String, required: true, unique: true}
+    name : {
+        type: String, 
+        required: true,
+    }, 
+    age: Number, 
+
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+        validate: {
+            validator: function(v) {
+                const user = await mongoose.models.User.findOne({email: value});
+                if(this._id) {
+                    if(user && user._id.toString() !== this._id.toString()) {
+                        return false;
+                    }
+                }
+                return !user;
+            }, 
+            message: 'Email already exists'
+        }
+    }
 });
+
 module.exports = mongoose.model('User', userSchema);
