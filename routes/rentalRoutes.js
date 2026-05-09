529e25186112240d60a930f00639bb053d63cc58
Shivam Kumar Chaubey <shivamc36@gmail.com>
2026-05-09 10:30:45 +0530

    Removed Fawn Library and used moongoose own session.startTransaction

--- routes/rentalRoutes.js
+++ routes/rentalRoutes.js
@@ -1,42 +1,67 @@
 const express = require('express');
 const router = require('express').Router();
+
+const mongoose = require('mongoose');
+
 const Movie = require('../models/Movie');
 const Genre = require('../models/Genre');
+const Rental = require('../models/Rental');
+
 const validateMovie = require('../middlewares/validateMovie');
 const validateRental = require('../middlewares/validateRental');
-const Rental = require('../models/Rental');
-const Fawn = require('fawn'); 
-const { default: mongoose } = require('mongoose');
 
-Fawn.init(mongoose);
 
 // GET all rentals
-router.get('/data', async(req, res) => {
-    try{
-        const rentals = await Rental.find().populate('movie', 'title').populate('user', 'name');
+router.get('/data', async (req, res) => {
+    try {
+        const rentals = await Rental.find()
+            .populate('movie', 'title')
+            .populate('user', 'name');
+
         res.json(rentals);
-    } catch(err) {
-        res.status(500).json({error: err.message});
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 });
 
+
 // Create a new rental
-router.post('/data', validateRental, async(req, res) => {
+router.post('/data', validateRental, async (req, res) => {
+
+    const session = await mongoose.startSession();
+
     try {
+
+        session.startTransaction();
+
         const { movieId, userId, rentalDate } = req.body;
+
         const rental = new Rental({
             movie: movieId,
             user: userId,
             rentalDate: rentalDate || Date.now()
         });
-        new Fawn.Task()
-            .save('rentals', rental)
-            .run();
+
+        await rental.save({ session });
+
+        await session.commitTransaction();
+
         res.status(201).json(rental);
-    }
-    catch(err) {
-        res.status(400).json({error: err.message});
+
+    } catch (err) {
+
+        await session.abortTransaction();
+
+        res.status(400).json({
+            error: err.message
+        });
+
+    } finally {
+
+        session.endSession();
+
     }
 });
 
-module.exports = router; 
+module.exports = router;
